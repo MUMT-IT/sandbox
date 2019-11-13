65d5ceec2107d03ad3b294cfb05b94575e0696e6
napatsakorn <napatsakorn.pis@mahidol.ac.th>
2019-11-13 17:42:16 +0700

    update function to support worktime data

--- otcalculator.py
+++ otcalculator.py
@@ -4,204 +4,97 @@ import numpy as np
 from datetime import datetime, timedelta, time
 import time
 import pandas as pd
-import numpy as np
-start_time = time.time() # measure execution time
 F = '%H:%M:%S'
-D = '%y-%m-%d'
 
-'''
-def readdate(): #read only same date for case of multi-datetime from excel
-    log = pd.read_excel('work_time.xlsx')
-    firstrow = str(log['checkin'][0])[:10]
-    for i in log.index:
-        getdate = str(log['checkin'][i])[:10]
-        #print (str(getdate)[:10])
-        if getdate == firstrow:
-            print ('Same day')
-        else:
-            print ('Different date time')
-'''
-
-
-def read_data(exel_file):
-    """Read data from a given excel file.
-
+"""Read data from a given excel file.
     excel_file: a filename
     """
-
-    log = pd.read_excel(exel_file)
-    log.dropna(inplace = True)
-    checkin = log['checkin']
-    checkout = log['checkout']
-    log2 = pd.DataFrame(log)
-    log2['Result Date'] = np.where(log2['checkin'].dt.date ==log2['checkout'].dt.date, 'same day', 'different day')
-    #print datetime.strptime(log2.checkin.apply(str), '%y-%m-%d %H:%M:%S')
-    print timedelta(hours=8, minutes=30)
-    #log2['Ealier OT'] = np.where(log2['checkin'].dt.time < log2['checkin'].dt.date.time.replace(hour=8, minute=30).strftime(D) , 'have OT' , 'not have OT')
-    print log2
-    return checkin, checkout
-    #setin = '08:30:00'
-    #dtype_setin = datetime.strptime(setin, F)
-    #t = checkin.dt.time
-    #tt = checkout.dt.time
-    #a = datetime.strptime('08:30:00',F).time()
-
-def checkd(checkin,checkout):
-    din = checkin.dt.date
-    dout = checkout.dt.date
-    #d = din.equals(dout)
-    #d = dout.isin(din)
-    equaldate = din.eq(dout)
-    #for i in din:
-    return equaldate
+def read_data(exel_file):
+    df = pd.read_excel(exel_file)
+    return df
 
 
-def caltime(checkin_date,checkout_date):
-    print  equaldate
+def splittime(x):
+        if pd.isnull(x):
+            return np.nan
+        times = x.split()
+        if len(times) > 1:
+            chkin = times[0]
+            chkout = times[-1]
+            if chkin != chkout:
+                return '{},{}'.format(chkin,chkout)
+            else:
+                return '{},{}'.format(chkin,'')
+        elif len(times) == 1:
+            return '{},{}'.format(x,'')
 
 
-def checkdate():
-    log = pd.read_excel('work_time.xlsx')
-    for i in log.index:
-        d_in = log['checkin'][i].strftime(D)
-        d_out = log['checkout'][i].strftime(D)
-        datein = datetime.strptime(d_in,D)
-        dateout = datetime.strptime(d_out,D)
-        if datein == dateout :
-            calculate()
-        elif dateout > datein:
-            print ('Check out date more than check in date')
-        else:
-            print ('Check out date less than check in date')
-
-def calculate(setin='08:30:00', setout='16:30:00'):
-    log = pd.read_excel('work_time.xlsx')
-    for i in log.index:
-        print ('ID : %i' %log['id'][i])
-        #print ((log['checkin']))
-        checkin = log['checkin'][i] #get checkin data
-        s1 = checkin.strftime(F) #datetime to string type, get onlytime (F declare as global var)
-        t_checkin = datetime.strptime(s1, F) #string to datetime type because can't let datetime minus datetime
-        dtype_set_in = datetime.strptime(setin,F)
-        if t_checkin >= dtype_set_in :
-            print ('Work on time/late')
-        else:
-            ot = dtype_set_in - t_checkin
-            print ('Work early (overtime): %s' %ot)
-        checkout = log['checkout'][i]  # get checkout data
-        s2 = checkout.strftime(F)  # datetime to string type, get only time
-        t_checkout = datetime.strptime(s2, F)  # string to datetime type because can't let datetime minus datetime
-        dtype_set_out = datetime.strptime(setout, F)
-        if t_checkout <= dtype_set_out:
-            print ('Get off early/on time')
-        else:
-            ot = t_checkout - dtype_set_out
-            print ('Night work overtime: %s' %ot)
-
-def allinone(checkin,checkout,setin='08:30:00', setout='16:30:00'):
-    d_in = checkin.strftime(D)
-    d_out = checkout.strftime(D)
-    datein = datetime.strptime(d_in,D)
-    dateout = datetime.strptime(d_out,D)
-    if datein == dateout:
-        # print ((log['checkin']))
-        s1 = checkin.strftime(F)  # datetime to string type, get onlytime (F declare as global var)
-        t_checkin = datetime.strptime(s1, F)  # string to datetime type because can't let datetime minus datetime
-        dtype_set_in = datetime.strptime(setin, F)
-        if t_checkin >= dtype_set_in:
-            return 'Work on time/late'
-        else:
-            ot = dtype_set_in - t_checkin
-            return 'Work early (overtime): {}'.format(ot)
-        s2 = checkout.strftime(F)  # datetime to string type, get only time
-        t_checkout = datetime.strptime(s2, F)  # string to datetime type because can't let datetime minus datetime
-        dtype_set_out = datetime.strptime(setout, F)
-        if t_checkout <= dtype_set_out:
-            return 'Get off early/on time'
-        else:
-            ot = t_checkout - dtype_set_out
-            return 'Night work overtime: {}'.format(ot)
-    elif dateout > datein:
-        return ('Check out date more than check in date')
-    else:
-        return ('Check out date less than check in date')
+def calculate_work_hours(df):
+    cleaned_df = df
+    cleaned_df[['chkin', 'chkout']] = df.Time.apply(splittime).dropna().str.split(',', expand=True)
+    cleaned_df['chkin'] = pd.to_datetime(cleaned_df['chkin'])
+    cleaned_df['chkout'] = pd.to_datetime(cleaned_df['chkout'])
+    cleaned_df['diff_time'] = cleaned_df['chkout'] - cleaned_df['chkin']
+    cleaned_df['diff_mins'] = cleaned_df['diff_time'] / np.timedelta64(1, 'm')
+    cleaned_df['diff_hours'] = cleaned_df['diff_time'] / np.timedelta64(1, 'h')
+    return cleaned_df
 
 
-def caldate(checkin, checkout, setin='08:30:00', setout='16:30:00'):
-    if not checkout:
-        raise ValueError
-
-    if checkin.date() == checkout.date():
-        s1 = checkin.strftime(F)  # datetime to string type, get onlytime (F declare as global var)
-        t_checkin = datetime.strptime(s1, F)  # string to datetime type because can't let datetime minus datetime
-        dtype_set_in = datetime.strptime(setin, F)
-        if t_checkin >= dtype_set_in:
-            return 'Work on time/late'
-        else:
-            ot = dtype_set_in - t_checkin
-            return 'Work early'
-    # return value if late show - value
-        checkout = log['checkout'][i]  # get checkout data
-        s2 = checkout.strftime(F)  # datetime to string type, get only time
-        t_checkout = datetime.strptime(s2, F)  # string to datetime type because can't let datetime minus datetime
-        dtype_set_out = datetime.strptime(setout, F)
-
-        if t_checkout <= dtype_set_out:
-            return 'Get off early/on time'
-        else:
-            ot = t_checkout - dtype_set_out
-            return 'Night work overtime: {}'.format(ot)
-
-    elif checkin.date() < checkout.date():
-        return ('Check out date more than check in date')
-    else:
-        return ('Check out date less than check in date')
+def workhourstatus (setin, setout,work_hours):
+    setin = pd.to_datetime(setin)
+    setout = pd.to_datetime(setout)
+    work_hours['chkin_status'] = work_hours.chkin.apply(lambda x: 'late' if x>setin else 'ontime')
+    work_hours['chkout_status'] = work_hours.chkout.apply(lambda x: 'on time' if x > setout else 'Early')
+    return work_hours
 
 
-def calculate_work_hours(checkin_times, checkout_times):
-    return calculate_ot_hours(checkin_times, checkout_times)
+def write_to_excel(df):
+    export_excel =df.to_excel(r'/Users/nut/Desktop/ot_result.xls',header=True)
+    return export_excel
 
 
-def create_datetime(row, hour, minute, second=0):
-    return datetime(row.year, row.month, row.day,
-                    hour, minute, second)
+def read_schedule(excel_work_file):
+    df = pd.read_excel(excel_work_file)
+    return df
 
 
-def round_hours(deltahour):
-    if deltahour >= 0:
-        return math.floor(deltahour)
-    else:
-        return math.floor(abs(deltahour)) * -1
+def calculate_ot (work_hours, ot_schedule):
+    work_hours = read_data()
+    ot_schedule = read_schedule()
+    df1 = DataFrame(work_hours, columns=['ID'])
+    df2 = DataFrame(ot_schedule, columns=['ID'])
+    df1['ID'] = df2['ID']
+    df1.Product.apply(lambda x: df2.AttrName[df2.ProdDetail.str.contains(x)], 1)
 
 
-def calculate_ot_hours(scantimes,
-                       check_hour,
-                       check_minute,
-                       check_second=0):
-    """Calculate OT hours for same date.
-
-    scantimes: Series of check-in times
-    check_hour: hour
-    check_minute: minute
-    check_second: second
-    return: Data frame of OT hours
-    """
-    checktimes = scantimes.apply(create_datetime, args=[
-        check_hour, check_minute, check_second
-    ])
-    ot_time = scantimes - checktimes
-    ot_hours = ot_time/pd.Timedelta(hours=1)
-    return ot_hours.apply(round_hours)
-
-
+def calculate_ot_time(chkin, chkout, shiftstart, shiftend):
+    chkin = datetime.strptime(chkin,F)
+    shiftstart = datetime.strptime(shiftstart, F)
+    chkout = datetime.strptime(chkout, F)
+    shiftend = datetime.strptime(shiftend, F)
+    if shiftstart >= chkin:
+        if chkout >= shiftend: #checkin before and checkout after OT set time
+            ot = shiftend - shiftstart
+            ot_minute = ot.seconds / 60
+            print ot_minute
+        else: #checkin before shiftstart but checkout before shiftend
+            ot = chkout - shiftstart
+            ot_minute = ot.seconds / 60
+            print ot_minute
+    else: #checkin after shiftstart
+        if chkout >= shiftend:  #checkin after shiftstart but checkout after shiftend
+            ot = shiftend - chkin
+            ot_minute = ot.seconds / 60
+            print ot_minute
+        else: #checkin after shiftstart and checkout before shiftend
+            ot = chkout - chkin
+            ot_minute = ot.seconds / 60
+            print ot_minute
+    #return 180
 
 if __name__ == '__main__':
-    #caldate()
-    #readdata()
-    excel_file = sys.args[1]
-    checkin,checkout = read_data(excel_file)
-    checkd(checkin,checkout)
-    #equaldate = checkd(checkin,checkout)
-    #caltime(checkin,checkout,equaldate)
-    #allinone(checkin,checkout)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    #excel_file = read_data('/Users/nut/Desktop/reportworktime.xls')
+    #work_hours = calculate_work_hours(excel_file)
+    #workhourstatus('09:00:00', '16:00:00',work_hours)
+    #excel_file = read_schedule('/Users/nut/Desktop/workschedule.xls')
+    calculate_ot_time(chkin='05:30:00', chkout='9:59:00', shiftstart='06:00:00', shiftend='09:00:00')
